f48216225402e67c20ba8123ef326b87d11ca4e7
MiqayelGH <98755696+MiqayelGH@users.noreply.github.com>
2022-08-10 21:14:53 +0400

    fibonachi and factorial with caching

--- hmwrk.js
+++ hmwrk.js
@@ -1,21 +1,21 @@
 function fact(n) {
-  if (n <= 1) return 1
+  if (n <= 1) return 1;
   fact.cache = fact.cache ?? [];
 
   if (!fact.cache[n]) {
     fact.cache[n] = n * fact(n - 1);
-  }
+  };
 
-  return fact.cache[n]
+  return fact.cache[n];
 }
 
 function fcFib(n) {
-  if (n <= 1) return 1
+  if (n <= 1) return 1;
   fcFib.cache = fcFib.cache ?? [];
 
   if (!fcFib.cache[n]) {
     fcFib.cache[n] = fcFib(n - 1)  + fcFib(n - 2);
-  }
+  };
 
-  return fcFib.cache[n]
+  return fcFib.cache[n];
 }
